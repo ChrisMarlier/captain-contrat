8498496c47608f323ffba1ccd433792f2e4a4179
ChrisMarlier <37925113+ChrisMarlier@users.noreply.github.com>
2024-04-19 18:13:31 +0200

    Add simple seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Warrior.create!(name: 'Conan', attack: 10, defense: 5, health: 100)
+Warrior.create!(name: 'Thor', attack: 8, defense: 7, health: 100)
+
+Fight.create!(warrior1: Warrior.first, warrior2: Warrior.last).start
